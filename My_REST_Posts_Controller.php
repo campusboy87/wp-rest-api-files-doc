8b9a8fea78a49ffd58f79c1568737e6634b65942
Kama <tkama@bk.ru>
2018-09-05 03:07:23 +0500

    Update My_REST_Posts_Controller.php

--- My_REST_Posts_Controller.php
+++ My_REST_Posts_Controller.php
@@ -1,62 +1,57 @@
 class My_REST_Posts_Controller {
 
-	// Here initialize our namespace and resource name.
-	public function __construct() {
+	## инициализация, во время которой указываем namespace и resource_name
+	function __construct() {
 		$this->namespace     = '/my-namespace/v1';
 		$this->resource_name = 'posts';
 	}
 
-	// Register our routes.
-	public function register_routes() {
+	## регистрация маршрутов
+	function register_routes() {
+
 		register_rest_route( $this->namespace, '/' . $this->resource_name, array(
-			// Here we register the readable endpoint for collections.
+			// конечная точка для чтения коллекции ресурсов
 			array(
 				'methods'   => 'GET',
 				'callback'  => array( $this, 'get_items' ),
 				'permission_callback' => array( $this, 'get_items_permissions_check' ),
 			),
-			// Register our schema callback.
+			// схема ресурса
 			'schema' => array( $this, 'get_item_schema' ),
 		) );
+
 		register_rest_route( $this->namespace, '/' . $this->resource_name . '/(?P<id>[\d]+)', array(
-			// Notice how we are registering multiple endpoints the 'schema' equates to an OPTIONS request.
+			// конечная точка для чтения отдельного ресурса
 			array(
 				'methods'   => 'GET',
 				'callback'  => array( $this, 'get_item' ),
 				'permission_callback' => array( $this, 'get_item_permissions_check' ),
 			),
-			// Регистрируем функцию, которая сгенерирует схему.
+			// схема ресурса
 			'schema' => array( $this, 'get_item_schema' ),
 		) );
 	}
 
-	/**
-	 * Проверяет доступ к чтению записей.
-	 *
-	 * @param WP_REST_Request $request Current request.
-	 *
-	 * @return bool|WP_Error
-	 */
-	public function get_items_permissions_check( $request ) {
+	## Проверяет доступ к чтению ресурсов.
+	function get_items_permissions_check( $request ) {
 		if ( ! current_user_can( 'read' ) ) {
-			return new WP_Error( 'rest_forbidden', 'Вы не можете просмотреть ресурс записи', array( 'status' => $this->authorization_status_code() ) );
+			return new WP_Error( 'rest_forbidden', 'Вы не можете просмотреть ресурс записи', array( 'status' => $this->error_status_code() ) );
 		}
 
 		return true;
 	}
 
 	/**
-	 * Grabs the five most recent posts and outputs them as a rest response.
+	 * Получает последние посты и отдает их в виде rest ответа.
 	 *
-	 * @param WP_REST_Request $request Current request.
+	 * @param WP_REST_Request $request Текущий запрос.
 	 *
-	 * @return mixed|WP_REST_Response
+	 * @return WP_Error|WP_REST_Response
 	 */
-	public function get_items( $request ) {
-		$args = array(
+	function get_items( $request ) {
+		$posts = get_posts( array(
 			'post_per_page' => 5,
-		);
-		$posts = get_posts( $args );
+		) );
 
 		$data = array();
 
@@ -69,79 +64,59 @@ class My_REST_Posts_Controller {
 			$data[] = $this->prepare_response_for_collection( $response );
 		}
 
-		// Return all of our comment response data.
 		return rest_ensure_response( $data );
 	}
 
-	/**
-	 * Check permissions for the posts.
-	 *
-	 * @param WP_REST_Request $request Current request.
-	 *
-	 * @return bool|WP_Error
-	 */
-	public function get_item_permissions_check( $request ) {
-		if ( ! current_user_can( 'read' ) ) {
-			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the post resource.' ), array( 'status' => $this->authorization_status_code() ) );
-		}
+	## Проверка права доступа.
+	function get_item_permissions_check( $request ) {
+		if ( ! current_user_can( 'read' ) )
+			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the post resource.' ), array( 'status' => $this->error_status_code() ) );
+
 		return true;
 	}
 
 	/**
-	 * Grabs the five most recent posts and outputs them as a rest response.
+	 * Получает отдельный ресурс.
 	 *
-	 * @param WP_REST_Request $request Current request.
+	 * @param WP_REST_Request $request Текущий запрос.
 	 *
-	 * @return mixed|WP_REST_Response
+	 * @return WP_Error|WP_REST_Response
 	 */
-	public function get_item( $request ) {
+	function get_item( $request ) {
 		$id = (int) $request['id'];
 		$post = get_post( $id );
 
-		if ( empty( $post ) ) {
+		if ( empty( $post ) )
 			return rest_ensure_response( array() );
-		}
 
-		$response = prepare_item_for_response( $post );
-
-		// Return all of our post response data.
-		return $response;
+		return prepare_item_for_response( $post, $request );
 	}
 
 	/**
-	 * Matches the post data to the schema we want.
+	 * Собирает данные ресурса в соответствии со схемой ресурса.
 	 *
-	 * @param WP_Post $post The comment object whose response is being prepared.
+	 * @param WP_Post         $post    Объект ресурса, из которого будут взяты оригинальные данные.
+	 * @param WP_REST_Request $request Текущий запрос.
 	 *
-	 * @return mixed|WP_REST_Response
+	 * @return WP_Error|WP_REST_Response
 	 */
-	public function prepare_item_for_response( $post, $request ) {
+	function prepare_item_for_response( $post, $request ) {
 		$post_data = array();
 
-		$schema = $this->get_item_schema( $request );
+		$schema = $this->get_item_schema();
 
 		// We are also renaming the fields to more understandable names.
-		if ( isset( $schema['properties']['id'] ) ) {
+		if ( isset( $schema['properties']['id'] ) )
 			$post_data['id'] = (int) $post->ID;
-		}
 
-		if ( isset( $schema['properties']['content'] ) ) {
+		if ( isset( $schema['properties']['content'] ) )
 			$post_data['content'] = apply_filters( 'the_content', $post->post_content, $post );
-		}
 
 		return rest_ensure_response( $post_data );
 	}
 
-	/**
-	 * Prepare a response for inserting into a collection of responses.
-	 *
-	 * This is copied from WP_REST_Controller class in the WP REST API v2 plugin.
-	 *
-	 * @param WP_REST_Response $response Response object.
-	 *
-	 * @return array|WP_REST_Response Response data, ready for insertion into collection data.
-	 */
-	public function prepare_response_for_collection( $response ) {
+	## Подготавливает ответ отдельного ресурса для добавления его в коллекцию ресурсов.
+	function prepare_response_for_collection( $response ) {
 		if ( ! ( $response instanceof WP_REST_Response ) ) {
 			return $response;
 		}
@@ -162,19 +137,15 @@ class My_REST_Posts_Controller {
 		return $data;
 	}
 
-	/**
-	 * Get our sample schema for a post.
-	 *
-	 * @return array
-	 */
-	public function get_item_schema() {
+	## Схема ресурса.
+	function get_item_schema() {
 		$schema = array(
-			// This tells the spec of JSON Schema we are using which is draft 4.
+			// // показывает какую версию схемы мы используем - это draft 4
 			'$schema'              => 'http://json-schema.org/draft-04/schema#',
-			// The title property marks the identity of the resource.
+			// определяет ресурс который описывает схема
 			'title'                => 'post',
 			'type'                 => 'object',
-			// In JSON Schema you can specify object properties in the properties attribute.
+			// в JSON схеме нужно указывать свойства в атрибуете 'properties'.
 			'properties'           => array(
 				'id' => array(
 					'description'  => esc_html__( 'Unique identifier for the object.', 'my-textdomain' ),
@@ -192,23 +163,16 @@ class My_REST_Posts_Controller {
 		return $schema;
 	}
 
-	// Sets up the proper HTTP status code for authorization.
-	public function authorization_status_code() {
-
-		$status = 401;
-
-		if ( is_user_logged_in() ) {
-			$status = 403;
-		}
-
-		return $status;
+	## Устанавливает HTTP статус код для авторизации.
+	function error_status_code() {
+		return is_user_logged_in() ? 403 : 401;
 	}
+
 }
 
-// Function to register our new routes from the controller.
+// Запускаем наш контроллер и регистрируем маршруты
+add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
 function prefix_register_my_rest_routes() {
 	$controller = new My_REST_Posts_Controller();
 	$controller->register_routes();
 }
-
-add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
